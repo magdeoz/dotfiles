a1ef8c3648fc576e820ff9e70a2076c67b9801b9
magdeoz <$mail>
2016-05-24 16:51:18 -0500

    set position

--- .i3/panel/panel.sh
+++ .i3/panel/panel.sh
@@ -5,7 +5,7 @@ bg_info="#252525"
 bg_time="#BA5E57"
 
 conky -c ~/.i3/panel/conkyrc1 | dzen2 -p -ta c -e 'button3=' -fn 'Roboto Mono Light for Powerline Light-8' -bg '#252525' -h 18 -w 610 -x 370 &
-conky -c ~/.i3/panel/conkyrc | dzen2 -p -ta c -e 'button3=' -fn 'Roboto Mono Light for Powerline Light-8' -fg $fg -bg $bg_info -h 18 -w 220 -x 1145
+conky -c ~/.i3/panel/conkyrc | dzen2 -p -ta c -e 'button3=' -fn 'Roboto Mono Light for Powerline Light-8' -fg $fg -bg $bg_info -h 18 -w 305 -x 1070
 
 ##backup
 #i3status -c ~/.i3/panel/i3status2.conf | dzen2 -p -ta c -e 'button3=' -fn 'Roboto Mono Light for Powerline Light-8' -fg $fg -bg $bg_time -h 18 -w 150 -x 1215 &
