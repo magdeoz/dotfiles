4edfa9225e44bbe52cda459dc44c25ae4efd7811
magdeoz <hackmagodeoz@gmail.com>
2016-06-04 18:22:40 -0500

    volume: add muted status

--- .i3/bin/volume.sh
+++ .i3/bin/volume.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env zsh
 
-status=$(amixer get Master | grep 'Front Left:' | awk '{print $5}' | cut -d '[' -f 2 | cut -d ']' -f 1 | cut -d '%' -f 1)
+Volume=$(amixer get Master | grep 'Front Left:' | awk '{print $5}' | cut -d '[' -f 2 | cut -d ']' -f 1 | cut -d '%' -f 1)
+Status=$(amixer get Master | grep 'Front Left:' | awk '{print $6}' | cut -d '[' -f 2 | cut -d ']' -f 1 )
 
-if [[ $status -gt '100' ]]; then
-         Vol='100'
-         echo $Vol
+if [ $Status == off ]; then
+        Vol='-m'
 else
-         Vol=$status
+        if [[ $Volume -gt '100' ]]; then
+                Vol='100'
+        else
+                Vol=$Volume
+        fi
 fi
-
 
 # Get Status
 volnoti-show $Vol
