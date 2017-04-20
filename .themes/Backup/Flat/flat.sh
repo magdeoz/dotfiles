a7dbf0a6a2b21f9403ae95cf6b4321a15c8b6d59
magdeoz <hackmagodeoz@gmail.com>
2017-04-20 11:04:13 -0500

    bspwm: Update config and panel

---
+++ .themes/Backup/Flat/flat.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+source ~/.themes/Functions/panel_functions
+
+while :; do
+        echo "" \
+                "%{S1}%{l}%{F#ff917154}  $(focus)" \
+                "%{c}        $(ws) " \
+                "%{r} $(ncmpcpp) |" \
+                "$(cpu MEM) |" \
+                "$(drives) |" \
+                "$(volume) |" \
+                "$(batery) |" \
+                "%{F#ff917154}$(printf '%b' "\uf017") $(timeday DATE) | $(timeday HOUR) " \
+                "%{F-}%{B-}" || break
+        sleep .1
+done 2> /dev/null | lemonbar \
+        -g 1343x24+11+5 \
+        -B ${BG} \
+        -f ${font} \
+        -f ${FONT2} \
+        -f ${icons} \
+        -f ${iconfont} \
+        $1
